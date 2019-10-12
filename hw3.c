21705bae7eda63570d5e2364d2bb578e55d50619
clenel2 <clenel2@uic.edu>
2019-10-12 14:19:18 -0500

    added semicolon functionality

--- hw3.c
+++ hw3.c
@@ -56,10 +56,17 @@ int main() {
      int i = 0; // first argsarray counter
      int k = 0; // second argsarray counter
      int first = 1; // check if we should put the arg into the first or second argsarray
+     int pipe = 0; // check if there was a '|'
+     int semi = 0; // check if there was a ';'
      while (word) {
        // copy a word to the arg array
-       if(strcmp(word, "|") == 0) {
+       if((strcmp(word, "|") == 0) && first == 1) {
          first = 0;
+         pipe = 1;
+       }
+       else if((strcmp(word, ";") == 0) && first == 1) {
+         first = 0;
+         semi = 1;
        }
        else if(first == 1) {
          strcpy(argsarray1[i], word);
@@ -73,11 +80,6 @@ int main() {
        word = strtok(NULL, " ");
      }  // end adding to argsarrays
 
-     /* print out our array
-     int j = 0;
-     for (;j < i; j++){
-       printf("argsarray1[%d]: %s\n", j, argsarray1[j]);
-     }*/
      // exit shell program
      if(strcmp(argsarray1[0], "exit") == 0)
       break;
@@ -86,16 +88,21 @@ int main() {
      argsarray1[i] = 0;
      argsarray2[k] = 0;
      int pid = fork();
+     int pid2 = -1;
+     if(semi == 1) {
+       pid2 = fork();
+     }
      if (pid == 0) {
-       //printf("I am the child!  I have pid %d. I'm gonna exec %s!\n", getpid(), argsarray1[0]);
-       //sprintf(line, "/bin/%s", argsarray1[0]);
-       //printf("%s\n", line);
-       //execv(line, argsarray1);
        execv(argsarray1[0], argsarray1);
 
        // should only be here if exec didn't work
        printf("pid %d did not exec\n", getpid());
        exit(1); // kill child
+       }
+       else if(pid2 == 0) {  // execute args after the semicolon
+         execv(argsarray2[0], argsarray2);
+         printf("pid %d did not exec\n", getpid());
+         exit(1); // kill child
        }
        else {
        //printf("I am the parent.  I am waiting for my child %d to die.\n", pid);
@@ -103,72 +110,12 @@ int main() {
        wait(&status);
        //printf("My child has died with status %d.\n", WEXITSTATUS(status));
        printf("pid:%d status:%d\n", pid, WEXITSTATUS(status));
+       if(semi == 1) { // wait for second command
+         wait(&status);
+         printf("pid:%d status:%d\n", pid, WEXITSTATUS(status));
+       }
      }
      free(argsarray1[i]);
      free(argsarray2[k]);
    }
-/*
-    int i = 99;
-    while(1){
-      printf("%d bottles of beer on the wall\n", i);
-      printf("%d bottles of beer\n", i);
-      printf("Take one down, pass it around\n");
-      i = i - 1;
-      printf("%d bottles of beer on the wall\n", i);
-    }
-// execexample.c
-  char *argsarray[] = {"/bin/ls", "-l", (char *)0};
-
-  int pid = fork();
-  if (pid == 0) {
-    printf("Child with pid %d, about to exec ls\n", getpid());
-    execv(argsarray[0], argsarray);
-  } else {
-    printf("I am the parent.  I am waiting for my child %d to die.\n", pid);
-    int status;
-    wait(&status);
-    printf("My child has died with status %d. :(\n", WEXITSTATUS(status));
-  }
-
-// forkexample.c
-  int pid = fork();
-  if (pid == 0) {
-    printf("I am the child!  I have pid %d.\n", getpid());
-    exit(6);
-  } else {
-    printf("I am the parent.  I am waiting for my child %d to die.\n", pid);
-    int status;
-    wait(&status);
-    printf("My child has died with status %d. :(\n", WEXITSTATUS(status));
-  }
-*/
-
-/*
-// ioredirect.c
-  char buf1[20];
-  char buf2[20];
-  char buf3[20];
-  char buf4[20];
-
-  int fd1 = open("sampletext1.txt", O_RDONLY);
-  int fd2 = open("sampletext2.txt", O_RDONLY);
-
-  read(fd1, buf1, 19);
-  buf1[19] = '\0';
-  printf("%s\n", buf1);
-
-  read(fd2, buf2, 19);
-  buf2[19] = '\0';
-  printf("%s\n", buf2);
-
-  dup2(fd1, fd2);
-
-  read(fd1, buf3, 19);
-  buf3[19] = '\0';
-  printf("%s\n", buf3);
-
-  read(fd2, buf4, 19);
-  buf4[19] = '\0';
-  printf("%s\n", buf4);
-  */
 }
